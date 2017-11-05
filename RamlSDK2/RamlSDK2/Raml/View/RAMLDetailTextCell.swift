34034e377e0488a0ec848f77e3dada4c55c55dcd
lyz <linyize@gmail.com>
2017-11-05 17:18:03 +0800

    插入图片圆角，写死

--- RamlSDK2/RamlSDK2/Raml/View/RAMLDetailTextCell.swift
+++ RamlSDK2/RamlSDK2/Raml/View/RAMLDetailTextCell.swift
@@ -22,16 +22,34 @@ class RAMLDetailTextCell: UICollectionViewCell {
         contentView.backgroundColor = .clear
     }
     
+    func magazineLogo(image: UIImage) -> UIImage {
+        let imageBounds = CGRect(x: 0, y: 0, width: 15, height: 15)
+        UIGraphicsBeginImageContextWithOptions(imageBounds.size, false, 0)
+        let path = UIBezierPath(roundedRect: imageBounds.insetBy(dx:0, dy:0), cornerRadius: 3.0)
+        let context = UIGraphicsGetCurrentContext()
+        context!.saveGState()
+        path.addClip()
+        image.draw(in: imageBounds)
+        context!.restoreGState()
+        let roundedImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        return roundedImage!;
+    }
+    
     func checkAttachImageLoadFinish(image: UIImage, attachment: InnerLineImageAttachment) {
         guard let contentString = textNode?.contentString else {
             return
         }
+        
+        // magazine logo round corner
+        let magazineLogo = self.magazineLogo(image: image)
+        
         let range = NSRange(location: 0, length: contentString.length)
         textNode?.contentString?.enumerateAttribute(NSAttachmentAttributeName, in: range,
                                                     options: NSAttributedString.EnumerationOptions.reverse,
                                                     using: {[weak self, weak attachment] value, _, _ in
                                                         if let attach = value as? InnerLineImageAttachment, attach.imageURL == attachment?.imageURL {
-                                                            attachment?.image = image
+                                                            attachment?.image = magazineLogo
                                                             self?.textLabel.setNeedsDisplay()
                                                         }
         })
